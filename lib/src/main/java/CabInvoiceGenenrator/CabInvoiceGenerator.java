8e767c003e2a6e7147ec655513e2b0a1b0b990f2
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-23 14:09:41 +0530

    Added function to calculate the cab invoice

---
+++ lib/src/main/java/CabInvoiceGenenrator/CabInvoiceGenerator.java
@@ -0,0 +1,13 @@
+package CabInvoiceGenenrator;
+
+public class CabInvoiceGenerator {
+
+	private static final double COST_PER_KM = 10.0;
+	private static final int COST_PER_MINUTE = 1;
+	private static final double MIN_FARE = 5.0;
+
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * COST_PER_KM + time * COST_PER_MINUTE;
+		return Math.max(totalFare, MIN_FARE);
+	}
+}
